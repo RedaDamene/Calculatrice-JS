c8cb6192b719fff423a5ba4adeb730f7746fafe3
RedaDamene <damene.reda4@gmail.com>
2022-10-13 11:12:16 +0200

    Resolution bug d'affichage

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const buttons = document.querySelectorAll('.btn')
 const result = document.getElementById('result')
-const clear = document.getElementById('clear')
-const equal = document.getElementById('equal')
+
 buttons.forEach((btn => {
     btn.addEventListener('click', (e) => {
         result.textContent += e.target.id
     })
-
-    clear.addEventListener('click', () => {
-        result.textContent = ""
-    })
 }))
+
+clear.addEventListener('click', () => {
+    result.textContent = ""
+})
+
+equal.addEventListener('click', () => {
+    result.textContent = eval(result.textContent)
+})
